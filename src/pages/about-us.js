a488add2fcb74bd899d3c40d48734973c96b9f4f
Qaseem <qaseemramzan0@gmail.com>
2022-08-26 14:08:47 -0400

    qaseemzia

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { PageWrapper } from "~components/Core";
 import BreadCrumbSection from "~sections/about/BreadCrumb";
-// import AboutSection from "~sections/about/about";
+import AboutSection from "~sections/about/About";
 import FeatureSection from "~sections/about/Feature";
 import ContentSection from "~sections/about/Content";
 import TeamSection from "~sections/about/Team";
@@ -16,7 +16,7 @@ export default function About() {
         text="Our focus is always on finding the best people to work with. Our <br class='d-none d-md-block'/>
 bar is high, but you look ready to take on the challenge."
       />
-      {/* <AboutSection /> */}
+      <AboutSection />
       <FeatureSection />
       <ContentSection />
       <TeamSection />
